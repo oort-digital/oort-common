1a4ac0bd2fef0d2500fb31b11de039451f716ce9
g.olt <olt.egor@gmail.com>
2022-09-06 18:13:00 +0300

    224: site fix

--- sites/my-site/src/App.tsx
+++ sites/my-site/src/App.tsx
@@ -1,14 +1,24 @@
-import { IChain, MyButton } from 'oort-lib';
+import { IChain, logger, WalletConnectConnector } from 'oort-lib';
 
 function App() {
  
   // const chain: IChain = {
   //   name: ''
   // }
+
+  /*
+  const wc = new WalletConnectConnector(logger, [{
+    name: "rinkeby",
+        chainId: 4,
+        rpcUrl: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+        blockExplorer: 'https://rinkeby.etherscan.io',
+  }])
+  */
   
   return (
     <div className="App">
-      <MyButton >Click here!</MyButton>
+      {/* <MyButton >Click here!</MyButton> */}
+      hello world!
     </div>
   )
 }
--- sites/my-site/vite.config.ts
+++ sites/my-site/vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+
+  optimizeDeps: {
+    esbuildOptions: {
+        target: "es2020",
+        supported: { bigint: true },
+
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+            // NodeGlobalsPolyfillPlugin({
+            //     buffer: true,
+            //     process: true,
+            // }),
+            // NodeModulesPolyfillPlugin({  })
+        ]
+    },
+  },
+
+  // resolve: {
+  //   alias: {
+  //       process: "process/browser",
+  //       stream: "stream-browserify",
+  //       zlib: "browserify-zlib",
+  //       util: "util",
+  //   },
+  // }
 })
