0fab4b03843a2f6b58d9890d57df2d1d1458e761
g.olt <olt.egor@gmail.com>
2022-09-07 12:51:04 +0300

    224: fix

--- sites/my-site/src/App.tsx
+++ sites/my-site/src/App.tsx
@@ -1,19 +1,26 @@
 import { IChain, logger, WalletConnectConnector } from 'oort-lib';
 
+  try {
+    const wc = new WalletConnectConnector(logger, [{
+      name: "rinkeby",
+          chainId: 4,
+          rpcUrl: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+          blockExplorer: 'https://rinkeby.etherscan.io',
+    }])
+  }
+  catch(e) {
+    debugger
+    throw e
+  }
+  
+
 function App() {
  
   // const chain: IChain = {
   //   name: ''
   // }
 
-  /*
-  const wc = new WalletConnectConnector(logger, [{
-    name: "rinkeby",
-        chainId: 4,
-        rpcUrl: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-        blockExplorer: 'https://rinkeby.etherscan.io',
-  }])
-  */
+
   
   return (
     <div className="App">
