7f85005b16ca97559e5cb9069edd8d7e51945644
g.olt <olt.egor@gmail.com>
2022-09-09 20:17:50 +0300

    add useTheme

--- packages/ui/src/effects/index.ts
+++ packages/ui/src/effects/index.ts
@@ -1,3 +1,4 @@
 export { useScreenSize, defaultScreenBrakepoints, ScreenSize } from "./useScreenSize"
 export { useDeviceType, DeviceType } from "./useDeviceType"
+export { useTheme } from "./useTheme"
 export type { IScreenBrakepoints  } from "./useScreenSize"
