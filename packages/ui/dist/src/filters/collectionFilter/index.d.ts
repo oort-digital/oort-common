714816a85cfeb303550fc92b1d225a2a075857c8
g.olt <olt.egor@gmail.com>
2022-09-30 20:14:38 +0300

    dist

--- packages/ui/dist/src/filters/collectionFilter/index.d.ts
+++ packages/ui/dist/src/filters/collectionFilter/index.d.ts
@@ -1,4 +1,5 @@
 export { CollectionFilter } from './collectionFilter';
 export { CollectionFilterMobile } from './collectionFilterMobile';
 export { CollectionFilterStore } from './collectionFilterStore';
+export type { IItemSource, ICollectionFilterItem } from "./itemSource";
 //# sourceMappingURL=index.d.ts.map
