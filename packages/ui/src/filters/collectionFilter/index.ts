065e00c7182b96ae98606f8643fd28f00594ec7f
g.olt <olt.egor@gmail.com>
2022-09-30 20:11:51 +0300

    imports fix

--- packages/ui/src/filters/collectionFilter/index.ts
+++ packages/ui/src/filters/collectionFilter/index.ts
@@ -1,3 +1,4 @@
 export { CollectionFilter } from './collectionFilter'
 export { CollectionFilterMobile } from './collectionFilterMobile'
 export { CollectionFilterStore } from './collectionFilterStore'
+export type { IItemSource, ICollectionFilterItem } from "./itemSource"
