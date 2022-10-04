fba7efed7ae23008196f7cb6944f930e15a9e14c
g.olt <olt.egor@gmail.com>
2022-10-04 17:16:38 +0300

    dist

---
+++ packages/urlSearchParams/dist/types/index.d.ts
@@ -0,0 +1,5 @@
+export { NumRangeSearchParam, IntArrSearchParam, StrArrSearchParam, IntSearchParam, BoolSearchParam, BaseSearchParam } from "./searchParams";
+export type { URLSearchParamsSetter } from "./typesAndInterfaces";
+export type { ISerializer } from "./serializers";
+export { StringSerializer, NumRangeSerializer, BoolSerializer, IntSerializer, ArraySerializer } from "./serializers";
+//# sourceMappingURL=index.d.ts.map
