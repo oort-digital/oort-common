d23065ff82ebfba2eeeba78d00b45cc6687914d5
g.olt <olt.egor@gmail.com>
2022-10-04 17:15:45 +0300

    add more imports

--- packages/urlSearchParams/src/index.ts
+++ packages/urlSearchParams/src/index.ts
@@ -1,5 +1,4 @@
-export { NumRangeSearchParam, BaseSearchParam } from "./searchParams"
+export { NumRangeSearchParam, IntArrSearchParam, StrArrSearchParam, IntSearchParam, BoolSearchParam, BaseSearchParam } from "./searchParams"
 export type { URLSearchParamsSetter } from "./typesAndInterfaces"
-
 export type { ISerializer } from "./serializers"
 export { StringSerializer, NumRangeSerializer, BoolSerializer, IntSerializer, ArraySerializer } from "./serializers"
