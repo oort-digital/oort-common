df587803f2fce92832a96e2c03981bfb5d8141a3
g.olt <olt.egor@gmail.com>
2022-10-16 16:11:31 +0300

    remove collection filter import

--- packages/ui/src/filters/index.ts
+++ packages/ui/src/filters/index.ts
@@ -1,4 +1,6 @@
-export * from "./collectionFilter"
+// imported CollectionFilter throw strange exception
+// workaround - copy collectionFilter dir to your repository
+// export * from "./collectionFilter"
 export * from "./popover"
 export * from "./searchInput"
 export * from "./subTitle"
