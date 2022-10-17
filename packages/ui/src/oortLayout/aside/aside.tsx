9b1fa7340342069439631ba8ececf7c6dae03ea9
g.olt <olt.egor@gmail.com>
2022-10-18 01:05:11 +0300

    aside styles fix

--- packages/ui/src/oortLayout/aside/aside.tsx
+++ packages/ui/src/oortLayout/aside/aside.tsx
@@ -1,5 +1,5 @@
 import { FooterMenu } from "../footerMenu";
-import styles from './asideDesktop.module.less';
+import styles from './aside.module.less';
 import { LogoLink } from "../logoLink";
 import {  NavMenu } from "../navMenu";
 import { IAsideProps } from "./typesAndInterfaces";
@@ -9,7 +9,7 @@ interface IProps extends IAsideProps {
     showLogo: boolean
 }
 
-export const Aside = ({ className, navItems, web3, isActiveFunc, showLogo }: IProps) => <aside className={className}>
+export const Aside = ({ className, navItems, web3, isActiveFunc, showLogo }: IProps) => <aside className={`${styles.aside} ${className}`}>
     { showLogo && <LogoLink className={styles.logo} /> }
     <NavMenu className={styles.nav_menu} isActiveFunc={isActiveFunc} navItems={navItems}/>
     <FooterMenu className={styles.footer} web3={web3} />
