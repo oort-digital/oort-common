0c7475cc0667156a78916930cb9ace27cae84b13
g.olt <olt.egor@gmail.com>
2022-11-17 02:40:29 +0300

    logs

--- packages/ui/src/filters/collectionFilter/collectionFilterMobile.tsx
+++ packages/ui/src/filters/collectionFilter/collectionFilterMobile.tsx
@@ -1,3 +1,4 @@
+import { logger } from "@oort/logger"
 import { observer } from "mobx-react"
 import { useEffect } from "react"
 import { CollectionFilterContent } from "./collectionFilterContent"
@@ -12,17 +13,25 @@ export interface ICollectionFilterMobileProps extends ICommonProps {
 const Impl = ({ filterStore, searchable = true, searchPlaceholder, circleIcons = true,
 	addFilterEventListeners, removeFilterEventListeners, onChange, noClear = false }: ICollectionFilterMobileProps) => {
 
+	const debug = (msg: string) => {
+		logger.debug(`CollectionFilterMobile. ${msg}`)
+	}
+
 	const onClose = () => {
+		debug('onClose')
 		filterStore.clearNotApplied()
 	}
 
 	const onClear = () => {
+		debug('onClear')
 		onChange([])
 		filterStore.clearNotApplied()
 	}
 
 	const onSubmit = () => {
+		debug(`onSubmit1. selectedLen ${filterStore.selected.length}`)
 		filterStore.copyNotAppliedToRecent && filterStore.copyNotAppliedToRecent()
+		debug(`onSubmit2. selectedLen ${filterStore.selected.length}`)
 		const selectedItems = filterStore.all.filter(x => filterStore.selected.some(s => s === x.key))
 		onChange(selectedItems)
 		filterStore.clearNotApplied()
