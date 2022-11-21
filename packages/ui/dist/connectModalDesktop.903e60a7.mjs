468e0371369510ae22c95170aca41536b50009d9
g.olt <olt.egor@gmail.com>
2022-11-21 20:24:25 +0300

    build pks

--- packages/ui/dist/connectModalDesktop.903e60a7.mjs
+++ packages/ui/dist/connectModalDesktop.903e60a7.mjs
@@ -1,7 +1,7 @@
 import { useState as L } from "react";
-import { j as t, O as S, a as e, F as d, i as E } from "./index.632529a3.mjs";
+import { j as t, O as S, a as e, F as d, i as E } from "./index.e250694b.mjs";
 import { Row as I, Col as h } from "antd";
-import { A as G, B as R, a as k, C as p, b as U, M as q, W as z } from "./alert.802ba292.mjs";
+import { A as G, B as R, a as k, C as p, b as U, M as q, W as z } from "./alert.9278f10f.mjs";
 import "react-router-dom";
 import "ethers";
 import "mobx-react";
@@ -138,4 +138,4 @@ const H = "_modal_f97e5_5", J = "_description_f97e5_8", K = "_chain_buttons_f97e
 export {
   nt as default
 };
-//# sourceMappingURL=connectModalDesktop.21203bf0.mjs.map
+//# sourceMappingURL=connectModalDesktop.903e60a7.mjs.map
