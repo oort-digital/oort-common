ca0cfcfb53b2d455021ba8244f8d0e1b5f26b180
g.olt <olt.egor@gmail.com>
2022-11-26 02:22:22 +0300

    fix

--- packages/ui/src/effects/useTheme/utils.test.ts
+++ packages/ui/src/effects/useTheme/utils.test.ts
@@ -1,15 +1,16 @@
 import { getCookieDomain } from "./utils";
 
-test("getCookieDomain. oort.digital", async () => {
-    const host = 'https://dashboard-test.oort.digital/'
-    const expected = 'oort.digital'
-    const actual = getCookieDomain(host)
+const getCookieDomainTest = (host: string, expected: string) => {
+    let actual = getCookieDomain(host)
     expect(actual).toEqual(expected);
-});
+    actual = getCookieDomain(`${host}/`)
+    expect(actual).toEqual(expected);
+}
 
-test("getCookieDomain. netlify.app", async () => {
-    const host = 'https://63813a30ccf1f500082d462d--dashboard-test-env.netlify.app/'
-    const expected = '63813a30ccf1f500082d462d--dashboard-test-env.netlify.app'
-    const actual = getCookieDomain(host)
-    expect(actual).toEqual(expected);
+test("getCookieDomain. oort.digital", () => getCookieDomainTest('https://dashboard-test.oort.digital', 'oort.digital'))
+
+test("getCookieDomain. netlify.app", () => {
+    getCookieDomainTest(
+        '63813a30ccf1f500082d462d--dashboard-test-env.netlify.app',
+        '63813a30ccf1f500082d462d--dashboard-test-env.netlify.app')
 });
