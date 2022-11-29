10e097c91805785541279828f2fcc410cc529e34
g.olt <olt.egor@gmail.com>
2022-11-29 03:31:53 +0300

    static collection filter

---
+++ packages/ui/src/filters/collectionFilter/staticCollectionFilter.tsx
@@ -0,0 +1,55 @@
+import styles from "./collectionFilter.module.less"
+import { PopoverFilter } from "../popover"
+import { observer } from "mobx-react"
+import { CollectionFilterContent } from "./collectionFilterContent"
+import { SubTitle } from "../subTitle"
+import { useEffect, useState } from "react"
+import { TooltipPlacement } from "antd/es/tooltip"
+import { ICollectionFilterItem, ItemKeyType, SelectMode, StaticCollectionFilterStore } from "./stores"
+import { CollectionFilter } from "./collectionFilter"
+
+interface IProps {
+	items: ICollectionFilterItem[]
+	title: string
+	applied: ItemKeyType[]
+	onChange: (collections: ICollectionFilterItem[]) => void
+	searchable?: boolean
+	searchPlaceholder?: string
+    circleIcons?: boolean
+    noClear?: boolean
+	popoverTitle: string
+	visible?: boolean
+    showTriggerButton?: boolean
+    showClose?: boolean
+    showCancel?: boolean
+    showClear?: boolean
+	onVisibleChange?: (isVisible: boolean) => void
+	placement?: TooltipPlacement
+	selectMode?: SelectMode
+}
+
+const Impl = (props: IProps) => {
+
+	const selectMode = props.selectMode ?? SelectMode.Multy
+	const [ filterStore, setStore ] = useState<StaticCollectionFilterStore>()
+	useEffect(() => {
+		setStore(new StaticCollectionFilterStore(selectMode, props.items))
+	}, [props.items, selectMode])
+
+	useEffect(() => {
+		if(filterStore) {
+			filterStore.setApplied(props.applied)
+		}
+	}, [props.applied, filterStore])
+
+	if(!filterStore) {
+		return null
+	}
+
+	const mergedProps = { ...props, ...{ filterStore } }
+
+    return <CollectionFilter {...mergedProps} >
+	</CollectionFilter>
+}
+
+export const StaticCollectionFilter = observer(Impl)
