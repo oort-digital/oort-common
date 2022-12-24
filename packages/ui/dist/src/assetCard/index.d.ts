e8dcb10ae824708836ad5d5a4db8ad67d233e0d5
g.olt <olt.egor@gmail.com>
2022-12-24 23:47:58 +0300

    build

--- packages/ui/dist/src/assetCard/index.d.ts
+++ packages/ui/dist/src/assetCard/index.d.ts
@@ -1,5 +1,5 @@
 export { AssetCardLayout } from "./assetCardLayout";
 export { BorrowAssetCard } from "./borrowAssetCard";
 export { getNftName, getNftTitle } from "./utils";
-export type { IBorrowAssetItem } from "./borrowAssetCard";
+export type { IBorrowAssetItem, PriceMode } from "./borrowAssetCard";
 //# sourceMappingURL=index.d.ts.map
