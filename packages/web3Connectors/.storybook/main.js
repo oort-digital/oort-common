8b50d52f6672cdc61848bdfc53c9434bcc0b060a
g.olt <olt.egor@gmail.com>
2023-01-05 01:32:23 +0300

    storybook settings

---
+++ packages/web3Connectors/.storybook/main.js
@@ -0,0 +1,67 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    // "@storybook/preset-create-react-app",
+    {
+      name: '@storybook/preset-create-react-app',
+      options: {
+        craOverrides: {
+          fileLoaderExcludes: ['less'],
+        },
+      },
+    },
+    {
+      name: 'storybook-preset-less',
+      options: {
+        // cssLoaderOptions: {
+        //    modules: true,
+        //    localIdentName: '[name]__[local]--[hash:base64:5]',
+        // },
+        lessLoaderOptions: {
+          lessOptions: {
+            // strictMath: false,
+            // noIeCompat: true,
+            // relativeUrls: false,
+            javascriptEnabled: true,
+          }
+        }
+      }
+    }
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+
+  webpackFinal: config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        modules: [...config.resolve.modules],
+        fallback: {
+           stream: false,
+          //  timers: false,
+          //  tty: false,
+          //  os: false,
+          //  http: false,
+          //  https: false,
+          //  zlib: false,
+          //  util: false,
+           ...config.resolve.fallback,
+        }
+      }
+    };
+  },
+
+  // env: (config) => ({
+  //   ...config,
+  //   REACT_APP_ENV: require("../config/dev").REACT_APP_ENV,
+  // }),
+}
