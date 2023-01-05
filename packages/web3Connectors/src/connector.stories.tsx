5cfd1ff79c6720033f18b6088a9172f7f7e246eb
g.olt <olt.egor@gmail.com>
2023-01-05 20:59:20 +0300

    story fix

--- packages/web3Connectors/src/connector.stories.tsx
+++ packages/web3Connectors/src/connector.stories.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { WalletConnectConnector } from './walletConnectConnector';
 import { logger } from '@oort/logger';
+import { InjectedConnector } from './injectedConnector';
+import { IConnector } from './iConnector';
 
 const FakeComponent = () => <></>
 
@@ -35,30 +37,52 @@ const supportedChains = [
       }
   }]
 
-const connector = new WalletConnectConnector(logger, supportedChains)
+const walletConnect = new WalletConnectConnector(logger, supportedChains)
+const injected = new InjectedConnector(logger, supportedChains)
+
+type ConnectorType = 'walletConnect' | 'injected'
+
+
 
 const Template: ComponentStory<typeof FakeComponent> = (_args: any) => {
 
   const [ chainId, setChainId ] = useState<number>()
   const [ address, setAddress ] = useState<string>()
+  const [ connected, setConnected ] = useState<boolean>(false)
+
+  const [ curConnector, setCurConnector ] = useState<ConnectorType>('walletConnect')
+
+  const getConnectorInstance = (): IConnector => {
+    if(curConnector === 'injected') { return injected }
+    return walletConnect
+  }
 
   const onConnect = async () => {
-    const signer = await connector.getSigner()
+    const instance = getConnectorInstance()
+    const signer = await instance.getSigner()
     setAddress(await signer.getAddress())
     setChainId(await signer.getChainId())
+    setConnected(true)
+  }
+
+  const reset = () => {
+    setChainId(undefined)
+    setAddress(undefined)
+    setConnected(false)
   }
 
   useEffect(() => {
 
     const init = async () => {
-      if(await connector.isConnected) {
+      if(await getConnectorInstance().isConnected) {
         onConnect()
       }
     }
 
+    reset()
     init()
 
-  }, [])
+  }, [curConnector])
 
   /*
   connector.onChainChanged(_id => {
@@ -66,14 +90,27 @@ const Template: ComponentStory<typeof FakeComponent> = (_args: any) => {
   })*/
 
   const connect = async (chainId: number) => {
-    await connector.connect(chainId)
+    await getConnectorInstance().connect(chainId)
     onConnect()
   }
 
+  const disconnect = async () => {
+    await getConnectorInstance().disconnect()
+    reset()
+  }
+
   return <div>
+
+    <select value={curConnector} onChange={(ev: any) => setCurConnector(ev.target.value)}>
+      <option value="walletConnect">wallet connect</option>
+      <option value="injected">injected</option>
+    </select>
+
     <div>chainId: {chainId}</div>
     <div>address: {address}</div>
-    <button onClick={() => connect(1)}>Connect Ethereum</button>
+    <div>connected: {connected.toString()}</div>
+    <button disabled={connected} onClick={() => connect(1)}>Connect Ethereum</button>
+    <button disabled={!connected} onClick={() => disconnect()}>Disconnect</button>
 
   </div>
 }
