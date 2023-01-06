1b8788e1aa028b80a1d0e9570605e330981689a9
g.olt <olt.egor@gmail.com>
2023-01-07 00:56:42 +0300

    refcatoring

--- packages/ui/src/connectModal/connectModalDesktop.tsx
+++ packages/ui/src/connectModal/connectModalDesktop.tsx
@@ -1,104 +1,24 @@
-import { ReactNode, useState } from 'react'
-import { IChain, isChainEmpty } from "../typesAndInterfaces"
-import { Col, Row } from 'antd'
 import styles from "./connectModalDesktop.module.less"
-import { ConnectButton } from './connectButton';
-import { MetamaskIcon } from './metamaskIcon';
-import { WalletConnectIcon } from './walletConnectIcon';
-import { Gutter } from 'antd/lib/grid/row';
-import { ConnectorNames } from '@oort/web3-connectors';
 import { OortModal } from '../oortModal';
 import { IConnectModalProps } from './connectModal';
-import { ChainButtonWithLogic } from './chainButtonWithLogic';
-import { Alert } from './alert';
 import { Bold } from './bold';
+import { useConnectModalCommon } from './useConnectModalCommon';
 
-const ConnectModalDesktop = ({ web3, onCancel, visible, onClose, afterChainSwitch, afterConnect, expectedChainId }: IConnectModalProps) => {
+const ConnectModalDesktop = (props: IConnectModalProps) => {
 
-	const [ loading, setLoading ] = useState(false)
-	const {  supportedChains, chain, switchChain, canSwitchChain, connect, account, connectorName, supportedConnectors } = web3
-	
-	const connectAndClose = async (chainId: number, cnnName: ConnectorNames) => {
-		setLoading(true)
-        try {
-            if(await connect(chainId, cnnName)) {
-				onCancel && onCancel()
-				afterConnect && afterConnect()
-			}
-        }
-        finally {
-            setLoading(false)
-        }
-	}
+	const { footer1, footer2, content, onCancel, loading } = useConnectModalCommon({
+		props,
+		styles,
+		btnGutter: [10, 0],
+		isMobile: false
+	})
 
-	const switchChainAndClose = async (newChainId: number): Promise<void> => {		
-        setLoading(true)
-        try {
-            await switchChain(newChainId)
-			onCancel && onCancel()
-			afterChainSwitch && afterChainSwitch()
-        }
-        finally {
-            setLoading(false)
-        }
-    }
+	const { visible } = props
 
 	const footer = <>
-		<div>By connecting, I accept Oort Digital’s <a href='https://oort.digital/terms'>Terms of Service</a> and acknowledge</div>
-		<div>that you have read and understand the <a href='https://oort.digital/terms#disclaimer'>Oort Digital protocol disclaimer</a></div>
+		<div>{footer1}</div>
+		<div>{footer2}</div>
 	</>
-
-	const renderWalletBtn = (chainId: number, walletName: string, cnnName: ConnectorNames, icon: ReactNode) => {
-
-		if(account && cnnName === connectorName) {
-			return <ConnectButton
-					disabled={true}
-					walletName={walletName}
-					walletIcon={icon}
-					account={account}/>
-		}
-
-		const connector = supportedConnectors[cnnName]
-		if(!connector.isInstalled) {
-			return <ConnectButton
-				walletName={walletName}
-				onClick={() => window.open(connector.installUrl, '_blank')!.focus()}
-				walletIcon={icon}
-				labelText="Install"
-			/>
-		}
-
-		return <ConnectButton
-			walletName={walletName}
-			onClick={() => connectAndClose(chainId, cnnName)}
-			walletIcon={icon}
-			labelText="Connect"
-		/>
-
-	}
-
-	const renderChainBtn = (supportedChain: IChain) => {
-
-		const { chainId } = supportedChain
-	
-		return <Col flex={3} key={chainId}>
-			<ChainButtonWithLogic
-				onClick={() => switchChainAndClose(chainId)}
-				loading={loading}
-				expectedChainId={expectedChainId}
-				connectedChainId={chain.chainId}
-				canSwitchChain={canSwitchChain}
-				chain={supportedChain} />
-		</Col>
-	
-	}
-
-	const btnGutter: [Gutter, Gutter] = [10, 0]
-
-	const _onCancel = () => {
-		onClose && onClose()
-		onCancel && onCancel()
-	}
 	
 	return <OortModal 
 		loading={loading}
@@ -106,26 +26,8 @@ const ConnectModalDesktop = ({ web3, onCancel, visible, onClose, afterChainSwitc
 		className={styles.modal}
 		title='Network & Wallet'
 		width="690px"
-		visible={visible} onCancel={() => _onCancel()}>
-		<>
-			{
-				!isChainEmpty(chain) && <>
-					<div className={styles.description}>
-						<Alert account={account} chain={chain} supportedChains={supportedChains} expectedChainId={expectedChainId} />
-					</div>
-					<Row gutter={btnGutter} className={styles.chain_buttons} justify='space-between'>
-						{supportedChains.map(x => renderChainBtn(x))}
-					</Row>
-				</>
-			}
-			<div className={styles.description}>
-				<Bold>Connect your Wallet</Bold> and jump into the world of NFT's
-			</div>
-			<Row gutter={btnGutter} justify='space-between'>
-				<Col span={12}>{renderWalletBtn(supportedChains[0].chainId, "Metamask", ConnectorNames.Injected, MetamaskIcon)}</Col>
-				<Col span={12}>{renderWalletBtn(supportedChains[0].chainId, "WalletConnect", ConnectorNames.WalletConnect, WalletConnectIcon)}</Col>
-			</Row>
-		</>
+		visible={visible} onCancel={() => onCancel()}>
+		<>{content}</>
 	</OortModal>
 		
 	
--- packages/ui/src/connectModal/connectModalMobile.tsx
+++ packages/ui/src/connectModal/connectModalMobile.tsx
@@ -1,130 +1,26 @@
-import { ReactNode, useState } from 'react'
-import { Col, Row } from 'antd'
 import styles from "./connectModalMobile.module.less"
-
-import { ConnectButton } from './connectButton';
-import { MetamaskIcon } from './metamaskIcon';
-import { WalletConnectIcon } from './walletConnectIcon';
-import { Gutter } from 'antd/lib/grid/row';
-import { ConnectorNames } from "@oort/web3-connectors";
-import { IChain, isChainEmpty } from '../typesAndInterfaces';
 import { OortModalMobile } from '../oortModal';
-import { IConnectModalProps, IWeb3 } from './connectModal';
-import { ChainButtonWithLogic } from './chainButtonWithLogic';
-import { Alert } from './alert';
-import { Bold } from './bold';
+import { IConnectModalProps } from './connectModal';
+import { useConnectModalCommon } from './useConnectModalCommon';
 
-const ConnectModalMobile = ({ web3, visible, onCancel, onClose, afterConnect, afterChainSwitch, expectedChainId }: IConnectModalProps) => {
+const ConnectModalMobile = (props: IConnectModalProps) => {
 
-	const [ loading, setLoading ] = useState(false)
-	
-	const { supportedChains, chain, switchChain, canSwitchChain, connect, account, connectorName, supportedConnectors } = web3
+	const { footer1, footer2, content, onCancel, loading } = useConnectModalCommon({
+		props,
+		styles,
+		btnGutter: [0, 12],
+		isMobile: true
+	})
 
-	const connectAndClose = async (chainId: number, cnnName: ConnectorNames) => {
-		setLoading(true)
-        try {
-            await connect(chainId, cnnName)
-			onCancel && onCancel()
-			afterConnect && afterConnect()
-        }
-        finally {
-            setLoading(false)
-        }
-	}
+	const { visible } = props
 
-	const switchChainAndClose = async (newChainId: number): Promise<void> => {		
-        setLoading(true)
-        try {
-            await switchChain(newChainId)
-			onCancel && onCancel()
-			afterChainSwitch && afterChainSwitch()
-        }
-        finally {
-            setLoading(false)
-        }
-    }
+	const footer = <>{footer1}{footer2}</>
 
-	const footer = <>
-		By connecting, I accept Oort Digital’s <a href='https://oort.digital/terms'>Terms of Service</a> and acknowledge that you have read and understand the <a href='https://oort.digital/terms#disclaimer'>Oort Digital protocol disclaimer</a>
-	</>
-
-	const renderWalletBtn = (chainId: number, walletName: string, cnnName: ConnectorNames, icon: ReactNode) => {
-
-		if(account && cnnName === connectorName) {
-			return <ConnectButton
-					disabled={true}
-					walletName={walletName}
-					walletIcon={icon}
-					account={account}/>
-		}
-
-		const connector = supportedConnectors[cnnName]
-		if(!connector.isInstalled) {
-			return <ConnectButton
-				walletName={walletName}
-				onClick={() => window.open(connector.installUrl, '_blank')!.focus()}
-				walletIcon={icon}
-				labelText="Install"
-			/>
-		}
-
-		return <ConnectButton
-			walletName={walletName}
-			onClick={() => connectAndClose(chainId, cnnName)}
-			walletIcon={icon}
-			labelText="Connect"
-		/>
-
-	}
-
-	const renderChainBtn = (supportedChain: IChain) => {
-
-		const { chainId } = supportedChain
-	
-		return <Col span="24" key={chainId}>
-			<ChainButtonWithLogic
-				onClick={() => switchChainAndClose(chainId)}
-				loading={loading}
-				expectedChainId={expectedChainId}
-				connectedChainId={chain.chainId}
-				canSwitchChain={canSwitchChain}
-				chain={supportedChain} />
-		</Col>
-	
-	}
-
-	const btnGutter: [Gutter, Gutter] = [0, 12]
-
-	const _onCancel = () => {
-		onClose && onClose()
-		onCancel && onCancel()
-	}
-	
 	return <OortModalMobile viewMode="topGap" loading={loading} footer={footer}
 		className={styles.modal} title='Network & Wallet' visible={visible}
-		onCancel={() => _onCancel()}>
-		<>
-			{
-				!isChainEmpty(chain) && <>
-					<div className={styles.description}>
-						<Alert account={account} chain={chain} supportedChains={supportedChains} expectedChainId={expectedChainId} />
-					</div>
-					<Row gutter={btnGutter} className={styles.chain_buttons}>
-						{supportedChains.map(x => renderChainBtn(x))}
-					</Row>
-				</>
-			}
-			<div className={styles.description}>
-				<Bold>Connect your Wallet</Bold> and jump into the world of NFT's
-			</div>
-			<Row gutter={btnGutter}>
-				<Col span="24">{renderWalletBtn(supportedChains[0].chainId, "Metamask", ConnectorNames.Injected, MetamaskIcon)}</Col>
-				<Col span="24">{renderWalletBtn(supportedChains[0].chainId, "WalletConnect", ConnectorNames.WalletConnect, WalletConnectIcon)}</Col>
-			</Row>
-		</>
+		onCancel={() => onCancel()}>
+		<>{content}</>
 	</OortModalMobile>
-		
-	
-};
+}
 
 export default ConnectModalMobile
