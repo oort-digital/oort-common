b2a56216295eed7698d0345a27f8dca496e90c3d
g.olt <olt.egor@gmail.com>
2023-01-09 22:04:33 +0300

    web3 connectors build

--- packages/web3Connectors/dist/types/walletConnectConnector.d.ts
+++ packages/web3Connectors/dist/types/walletConnectConnector.d.ts
@@ -1,17 +1,23 @@
 import { ILogger } from "@oort/logger";
 import { BaseConnector, IChainInfo } from "./baseConnector";
 import { IConnector } from "./iConnector";
+export interface IWalletConnectOptions {
+    modalZIndex?: number;
+    projectId: string;
+    logger: ILogger;
+    chains: IChainInfo[];
+}
 export declare class WalletConnectConnector extends BaseConnector implements IConnector {
     private _walletConnect;
     private readonly _rpc;
-    constructor(logger: ILogger, chains: IChainInfo[]);
+    constructor({ logger, chains }: IWalletConnectOptions);
     disconnect(): Promise<void>;
     get canSwitchChain(): boolean;
-    switchChain(chainId: number): Promise<void>;
-    protected get rawProvider(): any;
+    switchChain(chainId: number): Promise<boolean>;
+    protected getRawProvider(): Promise<any>;
     get isConnected(): Promise<boolean>;
     get isInstalled(): boolean;
     get installUrl(): string;
-    enable(): Promise<any>;
+    connect(_chainId: number): Promise<boolean>;
 }
 //# sourceMappingURL=walletConnectConnector.d.ts.map
