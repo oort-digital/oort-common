8d49cc7187a368cf2bcc53ccc05b85a813e53921
g.olt <olt.egor@gmail.com>
2023-01-11 20:20:38 +0300

    web3Connectors - rollup config fix

--- packages/web3Connectors/rollup.config.mjs
+++ packages/web3Connectors/rollup.config.mjs
@@ -1,4 +1,49 @@
-import createConfig from "../../rollup.config.mjs";
 import pks from './package.json' assert { type: "json" };
+
+
+import esbuild from "rollup-plugin-esbuild";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+
+const input = "./src/index.ts";
+const plugins = [
+  json(),
+  esbuild({
+    minify: false,
+    tsconfig: "./tsconfig.json",
+    loaders: {
+      ".json": "json",
+    },
+  }),
+];
+
+function createConfig(packageName, packageDependencies) {
+  return [
+    {
+      input,
+      plugins,
+      external: Object.entries(packageDependencies).map(x => x[0]),
+      output: [
+        {
+          file: "./dist/index.cjs.js",
+          format: "cjs",
+          exports: "named",
+          name: packageName,
+          sourcemap: true,
+        },
+        {
+          file: "./dist/index.es.js",
+          format: "es",
+          exports: "named",
+          name: packageName,
+          sourcemap: true,
+        },
+      ],
+    }
+  ];
+}
+
+
+
 
 export default createConfig(pks.name, pks.dependencies);
