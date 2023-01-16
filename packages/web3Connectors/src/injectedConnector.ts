bb087eec0e1bb5f509e2ca3fdb288c6f454fed08
georgii.olt <georgii@oort.digital>
2023-01-16 23:15:39 +0300

    build fix

--- packages/web3Connectors/src/injectedConnector.ts
+++ packages/web3Connectors/src/injectedConnector.ts
@@ -1,4 +1,4 @@
-import { ILogger } from "@oort/logger";
+import { ILogger } from "@oort-digital/logger";
 import { BaseConnector, IChainInfo } from "./baseConnector";
 import { ConnectorNames } from "./connectorNames";
 import { IConnector } from "./iConnector";
