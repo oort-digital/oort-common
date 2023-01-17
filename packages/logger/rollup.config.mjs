29ed2c0969585fdb4a6fafa73bf5de7d2bb11017
georgii.olt <georgii@oort.digital>
2023-01-17 23:19:00 +0300

    logger upd rollup

--- packages/logger/rollup.config.mjs
+++ packages/logger/rollup.config.mjs
@@ -1,10 +1,5 @@
 import pks from "./package.json" assert { type: "json" }
 import createConfig from "../../rollup.config.mjs"
-
 import esbuild from "rollup-plugin-esbuild";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
 
-
-export default createConfig({ esbuild, nodeResolve, commonjs, json }, pks.name, Object.keys(pks.dependencies));
+export default createConfig({ esbuild }, pks.name, Object.keys(pks.dependencies));
