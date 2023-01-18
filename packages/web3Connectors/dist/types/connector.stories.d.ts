fda117b67f171340b396683d6f86d7eaa54a2dd3
georgii.olt <georgii@oort.digital>
2023-01-18 17:17:59 +0300

    remove web3 dist

--- packages/web3Connectors/dist/types/connector.stories.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-declare const _default: ComponentMeta<() => JSX.Element>;
-export default _default;
-export declare const Main: ComponentStory<() => JSX.Element>;
-//# sourceMappingURL=connector.stories.d.ts.map
--- packages/web3Connectors/dist/types/connectorNames.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum ConnectorNames {
-    Undefined = "",
-    Injected = "Injected",
-    WalletConnect = "WalletConnect",
-    FaceWallet = "FaceWallet"
-}
-//# sourceMappingURL=connectorNames.d.ts.map
