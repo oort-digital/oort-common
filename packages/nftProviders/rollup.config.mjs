c8967ad784141b7e45e66a236a2c8d103e4bd200
georgii.olt <georgii@oort.digital>
2023-01-20 15:16:21 +0300

    add more rollup plugins

--- packages/nftProviders/rollup.config.mjs
+++ packages/nftProviders/rollup.config.mjs
@@ -1,6 +1,8 @@
 import pks from "./package.json" assert { type: "json" }
 import createConfig from "../../rollup.config.mjs"
-
 import esbuild from "rollup-plugin-esbuild";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
-export default createConfig({ esbuild }, pks.name, Object.keys(pks.dependencies));
+
+export default createConfig({ esbuild, nodeResolve, commonjs }, pks.name, Object.keys(pks.dependencies));
