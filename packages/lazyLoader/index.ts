695c791c781513f3505a54702bca77c5b0c2729e
georgii.olt <georgii@oort.digital>
2023-02-26 19:07:20 +0100

    remove lazyLoader from UI

--- packages/lazyLoader/index.ts
+++
@@ -1,4 +0,0 @@
-export { useScreenSize, defaultScreenBrakepoints, ScreenSize } from "./useScreenSize"
-export type { IScreenBrakepoints } from "./useScreenSize"
-export { useDeviceType, DeviceType } from "./useDeviceType"
-export { LazyLoader } from "./lazyLoader"
