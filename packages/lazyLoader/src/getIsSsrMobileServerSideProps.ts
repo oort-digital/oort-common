910108a44878c1ef79f7e2027bfbeb06bcdd0d2e
georgii.olt <georgii@oort.digital>
2023-03-13 23:58:57 +0100

    logs

--- packages/lazyLoader/src/getIsSsrMobileServerSideProps.ts
+++ packages/lazyLoader/src/getIsSsrMobileServerSideProps.ts
@@ -1,3 +1,4 @@
+import { ILogger } from "@oort-digital/logger";
 import { IncomingHttpHeaders } from "http";
 
 interface IGetServerSidePropsContext {
@@ -6,17 +7,19 @@ interface IGetServerSidePropsContext {
     }
 }
 
-function getIsSsrMobile(context: IGetServerSidePropsContext): boolean {
+function getIsSsrMobile(context: IGetServerSidePropsContext, logger?: ILogger): boolean {
     const userAgent = context.req.headers["user-agent"] as string;
-    console.log(userAgent);
     //This will not give 100% coverage but should cover most of the commonly encountered devices.
     const isMobile = !!userAgent.match(/(iPhone|iPod|iPad|Android|webOS|BlackBerry|IEMobile|Opera Mini)/i);
+
+    logger?.debug(`clientUserAgent: ${userAgent}`);
+    logger?.debug(`isMobile: ${isMobile}`);
     return isMobile;
 }
 
 //call this function inside next.js getServerSideProps functions
-export function getIsSsrMobileServerSideProps(context: IGetServerSidePropsContext) {
+export function getIsSsrMobileServerSideProps(context: IGetServerSidePropsContext, logger?: ILogger) {
   return {
-    props: { isSsrMobile: getIsSsrMobile(context) }
+    props: { isSsrMobile: getIsSsrMobile(context, logger) }
   }
 }
