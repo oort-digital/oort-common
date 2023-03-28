90a7bb820516609a4be29c8ee530d22205a249d5
georgii.olt <georgii@oort.digital>
2023-03-28 15:42:17 +0530

    ui parser

--- packages/lazyLoaderNextJs/src/getSsrDeviceTypeProps.ts
+++ packages/lazyLoaderNextJs/src/getSsrDeviceTypeProps.ts
@@ -1,6 +1,8 @@
 import { DeviceType } from "@oort-digital/lazy-loader";
 import { ILogger } from "@oort-digital/logger";
 import { IncomingHttpHeaders } from "http";
+import { UAParser } from "ua-parser-js"
+import { toErrorWithMessage } from "@oort-digital/utils";
 
 interface IGetServerSidePropsContext {
     req: {
@@ -8,18 +10,48 @@ interface IGetServerSidePropsContext {
     }
 }
 
-function getSsrDeviceType(context: IGetServerSidePropsContext, logger?: ILogger): DeviceType {
-    const userAgent = context.req.headers["user-agent"] as string;
-    //This will not give 100% coverage but should cover most of the commonly encountered devices.
-    const isMobile = !!userAgent.match(/(iPhone|iPod|iPad|Android|webOS|BlackBerry|IEMobile|Opera Mini)/i);
+type UADeviceType = 'desktop' | 'mobile' | 'tablet'
+interface IDeviceInfo {
+  type?: UADeviceType
+}
 
-    logger?.debug(`clientUserAgent: ${userAgent}`);
-    logger?.debug(`isMobile: ${isMobile}`);
-    return isMobile ? DeviceType.Phone : DeviceType.Desktop;
+function parseDeviceType(device: IDeviceInfo, ua: string, logger: ILogger): DeviceType {
+  try {
+    if(device.type === 'mobile') {
+      return DeviceType.Phone
+    }
+  
+    if(device.type === 'tablet') {
+      return DeviceType.Tablet
+    }
+  }
+  catch(err) {
+    const errWithMessage = toErrorWithMessage(err)
+    logger?.error(`Can't parse device type.
+    UserAgent: ${ua}
+    ${errWithMessage.message}
+    `)
+  }
+  
+  return DeviceType.Desktop
+}
+
+function getSsrDeviceType(context: IGetServerSidePropsContext, logger: ILogger): DeviceType {
+    const userAgent = context.req.headers["user-agent"] as string;
+    logger.debug(`UA: ${userAgent}`);
+
+    const parser = new UAParser(userAgent)
+    const device: IDeviceInfo = parser.getDevice()
+    logger.debug(`device: ${JSON.stringify(device)}`);
+
+    const deviceType = parseDeviceType(device, userAgent, logger)
+    logger.debug(`deviceType: ${JSON.stringify(deviceType)}`);
+
+    return deviceType
 }
 
 //call this function inside next.js getServerSideProps functions
-export function getSsrDeviceTypeProps(context: IGetServerSidePropsContext, logger?: ILogger) {
+export function getSsrDeviceTypeProps(context: IGetServerSidePropsContext, logger: ILogger) {
   return {
     props: { ssrDeviceType: getSsrDeviceType(context, logger) }
   }
