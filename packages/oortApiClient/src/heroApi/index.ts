a0446f035f0b516c24150b1ffcf1e48a5cea15b9
georgii.olt <georgii@oort.digital>
2023-05-05 13:03:42 +0530

    more interfaces

--- packages/oortApiClient/src/heroApi/index.ts
+++ packages/oortApiClient/src/heroApi/index.ts
@@ -2,5 +2,5 @@ export { OortHeroApi } from "./oortHeroApi"
 export type { IOortHeroApi, IOpenBlindBoxParams, IQueryHeroesParams,
     IMintingQueryHeroesParams, IMintingHero, IHeroMintAvailableResponse,
     IHeroesResponse, IGameHeroesResponse, IOpenBlindBoxHero,
-    IHeroCampaignRewards
+    IHeroCampaignRewards, IHeroForMutate
 } from "./typesAndInterfaces"
