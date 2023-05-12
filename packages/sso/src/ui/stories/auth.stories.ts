66fd9dd629b8ff2389a71b1c2b232377314923ca
georgii.olt <georgii@oort.digital>
2023-05-12 22:29:35 +0300

    styles fix

--- packages/sso/src/ui/stories/auth.stories.ts
+++ packages/sso/src/ui/stories/auth.stories.ts
@@ -1,4 +1,5 @@
 import "../../../.storybook/index.less"
+import "@oort-digital/ui/dist/styles/theme/light.less"
 import type { Meta, StoryObj } from '@storybook/react';
 import { ConnectorNames } from "@oort-digital/web3-connectors";
 
