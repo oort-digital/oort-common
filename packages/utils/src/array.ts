5fb0bf1840e640e1ae90396619cd96ef8b797a65
georgii.olt <olt.egor@gmail.com>
2023-05-31 22:51:58 +0300

    more array utils

--- packages/utils/src/array.ts
+++ packages/utils/src/array.ts
@@ -12,3 +12,29 @@ export function distinct<T>(arr: Array<T>, keyFunc: (item: T) => string | number
 
   return result
 }
+
+type KeyType = number | string
+type Dictionary<T> = { [id: KeyType]: T }
+export function groupBy<T>(arr: T[], keyFunc: (item: T) => KeyType): Dictionary<T[]> {
+    const map: Dictionary<T[]> = {}
+    for(let i = 0; i < arr.length; i++) {
+        const key = keyFunc(arr[i]);
+        if(map[key]) {
+            map[key].push(arr[i])
+        }
+        else {
+            map[key] = [arr[i]]
+        }
+    }
+    return map
+}
+
+export function toDictionary<T>(arr: T[], keyFunc: (item: T) => KeyType): Dictionary<T> {
+    const map: Dictionary<T> = {}
+    for(let i = 0; i < arr.length; i++) {
+        const key = keyFunc(arr[i]);
+        map[key] = arr[i]
+    }
+    return map
+}
+
