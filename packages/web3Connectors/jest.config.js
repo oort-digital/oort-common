271823c09264a26dd69b068da8ad663403becc56
georgii.olt <olt.egor@gmail.com>
2023-06-05 20:39:35 +0300

    web3Connectors test fix

--- packages/web3Connectors/jest.config.js
+++ packages/web3Connectors/jest.config.js
@@ -1,9 +1,13 @@
 const config = {  
     transformIgnorePatterns: [
-      'node_modules/(?!(@web3modal)/)'
+      "node_modules/?!(@oort-digital)",
+      "node_modules/?!(@web3modal)"
     ],
     transform: {
       '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
+    },
+    moduleNameMapper: {
+      "^@oort-digital/utils(.*)$": "<rootDir>/node_modules/@oort-digital/utils/dist",
     },
     "resetMocks": false,
     "setupFiles": ["jest-localstorage-mock"]
