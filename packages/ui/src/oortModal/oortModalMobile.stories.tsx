ca8fb28e1bafe024109f5d9c1913bfc135a6a488
georgii.olt <olt.egor@gmail.com>
2023-06-08 20:07:42 +0300

    stories refactoring

--- packages/ui/src/oortModal/oortModalMobile.stories.tsx
+++ packages/ui/src/oortModal/oortModalMobile.stories.tsx
@@ -3,19 +3,29 @@ import "../styles/antOverride.less";
 import "../styles/fonts.css";
 import { Meta, StoryObj } from '@storybook/react';
 import { ThemeLoader } from "../internalHelpers";
-import { OortModal } from "./oortModal";
+import { OortModalMobile } from "./oortModalMobile"
 import { FooterButtons, IFooterBtn } from "./footerButtons";
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'oort/modal',
-  component: OortModal,
-} satisfies Meta<typeof OortModal>;
+  title: 'oort/modalMobile',
+  component: OortModalMobile,
+  parameters: {
+    //👇 The viewports object from the Essentials addon
+    viewport: {
+      //👇 The viewports you want to use
+      viewports: INITIAL_VIEWPORTS,
+      //👇 Your own default viewport
+      defaultViewport: 'iphone6',
+    },
+  },
+} satisfies Meta<typeof OortModalMobile>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const onCancel = (e: React.MouseEvent<HTMLElement>) => { alert('onCance') }
+const onCancel = () => { alert('onCance') }
 
 export const Primary: Story = {
   args: {
