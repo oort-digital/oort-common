c556900b993811d3048fc08cf73321e560d5e7fd
georgii.olt <olt.egor@gmail.com>
2023-06-20 23:22:21 +0300

    static collection fix

--- packages/ui/src/filters/collectionFilter/staticCollectionFilter.stories.tsx
+++ packages/ui/src/filters/collectionFilter/staticCollectionFilter.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../../stories.less";
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { InMemoryStoreStub } from "./testStores";
 
 import { ThemeLoader } from "../../internalHelpers";
@@ -9,52 +9,21 @@ import { BscIcon, PolygonIcon, EthIcon } from "../../icons";
 import { Button } from "antd";
 import { StaticCollectionFilter } from "./staticCollectionFilter";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'oort/filters/staticCollection',
-  component: StaticCollectionFilter,
 
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    // layout: 'fullscreen',
-  },
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof StaticCollectionFilter>;
+const meta = {
+	title: 'oort/filters/staticCollection',
+	component: StaticCollectionFilter,
+	
+} satisfies Meta<typeof StaticCollectionFilter>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
 
 
 const inMemoryStore = new InMemoryStoreStub({ useFavorites: true, useRecent: true, noIcons: false })
 inMemoryStore.setApplied([1, 2])
 
 const templateStyle = { height: '1000px' }
-
-const NoTriggerButtonTemplate: ComponentStory<typeof StaticCollectionFilter> = (args) => {
-
-	const [visible, onVisibleChange] = useState(false)
-
-	const [applied, setApplied] = useState([0])
-
-	const onChange = (collections: ICollectionFilterItem[]) => {
-		setApplied(collections.map(x => x.key as number))
-	}
-
-	const mergedProps = { ...args, ...{ visible, onVisibleChange, onChange, applied } }
-
-	return <>
-		<ThemeLoader />
-		<div style={templateStyle}>
-			<div>
-				selected: {JSON.stringify(applied)}
-			</div>
-			<Button onClick={() => onVisibleChange(true)}>Open</Button>
-			<StaticCollectionFilter {...mergedProps} />
-		</div>
-	</>
-}
-
-
 
 const getIcon = (i: number) => {
 
@@ -85,15 +54,40 @@ const generateItems = (count: number) => {
 }
 
 const items = generateItems(40)
-export const NoTriggerButton = NoTriggerButtonTemplate.bind({}); 
-NoTriggerButton.args = {
-	title: 'Title',
-	popoverTitle: 'PopoverTitle',
-	searchPlaceholder: 'Enter text',
-	items: items,
-	searchable: true,
-	showTriggerButton: false,
-    showClose: true,
-    showCancel: false,
-    showClear: true,
+export const NoTriggerButton: Story = {
+	args: {
+		title: 'Title',
+		popoverTitle: 'PopoverTitle',
+		searchPlaceholder: 'Enter text',
+		items: items,
+		searchable: true,
+		showTriggerButton: false,
+		showClose: true,
+		showCancel: false,
+		showClear: true,
+	},
+
+	render: (args) => {
+
+		const [visible, onVisibleChange] = useState(false)
+	
+		const [applied, setApplied] = useState([0])
+	
+		const onChange = (collections: ICollectionFilterItem[]) => {
+			setApplied(collections.map(x => x.key as number))
+		}
+	
+		const mergedProps = { ...args, ...{ visible, onVisibleChange, onChange, applied } }
+	
+		return <>
+			<ThemeLoader />
+			<div style={templateStyle}>
+				<div>
+					selected: {JSON.stringify(applied)}
+				</div>
+				<Button onClick={() => onVisibleChange(true)}>Open</Button>
+				<StaticCollectionFilter {...mergedProps} />
+			</div>
+		</>
+	}
 }
