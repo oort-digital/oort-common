dc24e2f93f98409c67276effe817933cbe2b96a0
wyk1991 <wyk511849791@163.com>
2023-07-11 15:23:53 +0800

    Fix the campaign menu text

--- packages/ui/src/oortLayout/stories/common.tsx
+++ packages/ui/src/oortLayout/stories/common.tsx
@@ -2,43 +2,46 @@ import { INavItems } from "../navMenu";
 import { TestCarousel } from "./testCarousel";
 
 export const navItems: INavItems = {
-    dashboard: 'http://dashboard.oort.local',
-    leaderboard: 'http://leaderboard.oort.local',
-    minting: {
-      mutation: {
-          reactRouterLink: false,
-          href: 'http://minting.oort.local/hero-mutation',
-          disabled: true,
-          // hide: true
-      },
-      claim: 'http://minting.oort.local/claim-rewards',
-      campaigns: 'http://minting.oort.local/campaigns',
+  dashboard: 'http://dashboard.oort.local',
+  leaderboard: 'http://leaderboard.oort.local',
+  minting: {
+    mutation: {
+      reactRouterLink: false,
+      href: 'http://minting.oort.local/hero-mutation',
+      disabled: true,
+      // hide: true
     },
-    rent: {
-      activity: 'http://rent.oort.local/avtivities',
-      borrow: 'http://rent.oort.local/borrow',
-      heroes: 'http://rent.oort.local/heroes',
-      lend: 'http://rent.oort.local/lend'
+    claimRewards: 'http://minting.oort.local/campaigns',
+  },
+  rent: {
+    activity: 'http://rent.oort.local/avtivities',
+    borrow: 'http://rent.oort.local/borrow',
+    heroes: 'http://rent.oort.local/heroes',
+    lend: 'http://rent.oort.local/lend'
+  },
+  gameHub: {
+    games: {
+      href: 'http://game-hub.oort.local/games',
+      reactRouterLink: true
     },
-    gameHub: {
-      games: {
-        href: 'http://game-hub.oort.local/games',
-        reactRouterLink: true
-      },
-      nfts: {
-        href: 'http://game-hub.oort.local/nfts',
-        reactRouterLink: true
-      }
-    },
-    rpgBattle: 'https://battle.oort.digital'  
+    nfts: {
+      href: 'http://game-hub.oort.local/nfts',
+      reactRouterLink: true
+    }
+  },
+  rpgBattle: 'https://battle.oort.digital'
 }
 
-export const testNavItems: INavItems = { ...navItems, ...{ developTools: {
-  faucet: 'http://develop.oort.local/faucet',
-  refreshMetadata: "http://develop.oort.local/refresh-metadata"
-}, } }
+export const testNavItems: INavItems = {
+  ...navItems, ...{
+    developTools: {
+      faucet: 'http://develop.oort.local/faucet',
+      refreshMetadata: "http://develop.oort.local/refresh-metadata"
+    },
+  }
+}
 
-const style = { 
+const style = {
   // background: 'red'
   height: '1000px'
 }
