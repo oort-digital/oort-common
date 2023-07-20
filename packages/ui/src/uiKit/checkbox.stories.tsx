a82760bc0f977f5d2b04e8ab934b59b5a75caedd
georgii.olt <olt.egor@gmail.com>
2023-07-20 23:37:34 +0300

    ui-kit stories

--- packages/ui/src/uiKit/checkbox.stories.tsx
+++ packages/ui/src/uiKit/checkbox.stories.tsx
@@ -1,43 +1,23 @@
-import React, { useState } from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { ThemeLoader } from "../internalHelpers";
+import { Meta, StoryObj } from "@storybook/react";
 
-import { Checkbox } from 'antd';
+import { Checkbox } from "antd";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'ui-kit/checkbox',
+const meta = {
+  title: "ui-kit/checkbox",
   component: Checkbox,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Checked: Story = {
+  args: {
+    checked: true,
   },
-} as ComponentMeta<typeof Checkbox>;
+};
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Checkbox> = (args) => {
-
-  const [checked, setChecked] = useState(true)
-
-  const internal = {
-    checked: checked,
-    onChange: () => setChecked(!checked)
-  }
-
-  const mergedArgs = { ...args, ...internal }
-
-  return <>
-    <ThemeLoader />
-    <div>
-      <Checkbox {...mergedArgs} >Checkbox</Checkbox>
-    </div>
-  </>
-
-}
-
-
-export const Primary = Template.bind({});
-  // More on args: https://storybook.js.org/docs/react/writing-stories/args
-  Primary.args = {
-
-  };
+export const UnChecked: Story = {
+  args: {
+    checked: false,
+  },
+};
