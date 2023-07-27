64dfe66c1c20df85834860f108d1961902abfb34
georgii.olt <olt.egor@gmail.com>
2023-07-27 14:24:56 +0300

    filter layout story fix

--- packages/ui/src/filters/layout/filterLayout.stories.tsx
+++ packages/ui/src/filters/layout/filterLayout.stories.tsx
@@ -1,39 +1,26 @@
 import React from "react";
 import "../../../.storybook/stories.less";
 
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { FilterLayout } from "./filterLayout";
-import { SearchInputMobile } from "../searchInput";
-import { ThemeLoader } from "../../internalHelpers";
+import { SearchInput } from "../searchInput";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: "oort/filters/layout/desktop",
   component: FilterLayout,
-
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    // layout: 'fullscreen',
-  },
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
-} as ComponentMeta<typeof FilterLayout>;
+} satisfies Meta<typeof FilterLayout>;
 
 export default meta;
 
-const Template: ComponentStory<typeof FilterLayout> = (args) => {
-  return (
-    <>
-      <ThemeLoader />
-      <FilterLayout {...args}>
-        <SearchInputMobile title="Search 1" onChange={() => {}} />
-        <SearchInputMobile title="Search 2" onChange={() => {}} />
-      </FilterLayout>
-    </>
-  );
-};
+type Story = StoryObj<typeof meta>;
 
-export const Main = Template.bind({});
-Main.args = {};
+const children = [
+  <SearchInput onChange={() => {}} />,
+  <SearchInput onChange={() => {}} />,
+];
+
+export const Primary: Story = {
+  args: {
+    children,
+  },
+};
--- packages/ui/src/filters/layout/filterLayoutMobile.stories.tsx
+++ packages/ui/src/filters/layout/filterLayoutMobile.stories.tsx
@@ -1,46 +1,31 @@
 import React from "react";
 import "../../../.storybook/stories.less";
 
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { FilterLayoutMobile } from "./filterLayoutMobile";
 import { SearchInputMobile } from "../searchInput";
-import { ThemeLoader } from "../../internalHelpers";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: "oort/filters/layout/mobile",
   component: FilterLayoutMobile,
-
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    // layout: 'fullscreen',
-  },
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
-} as ComponentMeta<typeof FilterLayoutMobile>;
+} satisfies Meta<typeof FilterLayoutMobile>;
 
 export default meta;
 
-const Template: ComponentStory<typeof FilterLayoutMobile> = (args) => {
-  return (
-    <>
-      <ThemeLoader />
-      <FilterLayoutMobile {...args}>
-        <SearchInputMobile title="Search 1" onChange={() => {}} />
-        <SearchInputMobile title="Search 2" onChange={() => {}} />
-      </FilterLayoutMobile>
-    </>
-  );
-};
+type Story = StoryObj<typeof meta>;
 
-export const Main = Template.bind({});
-Main.args = {
-  activeFiltersCounter: 1,
-  // sortBy?: ReactNode
-  // children: ChildType[] | ChildType
-  onApply: () => {},
-  onClearAll: () => {},
-  // onCancel?: () => {}
+const children = [
+  <SearchInputMobile title="Search 2" onChange={() => {}} placeholder="" />,
+  <SearchInputMobile title="Search 1" onChange={() => {}} placeholder="" />,
+];
+
+export const Primary: Story = {
+  args: {
+    activeFiltersCounter: 1,
+    // sortBy?: ReactNode
+    children,
+    onApply: () => {},
+    onClearAll: () => {},
+    // onCancel?: () => {}
+  },
 };
