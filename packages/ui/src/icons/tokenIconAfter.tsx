d17c3a936e335a32007ff4f65c8c3f4d4cceafcc
georgii.olt <olt.egor@gmail.com>
2023-08-01 12:54:13 +0300

    icon properties fix

--- packages/ui/src/icons/tokenIconAfter.tsx
+++ packages/ui/src/icons/tokenIconAfter.tsx
@@ -1,18 +1,57 @@
-export const TokenAfterIcon = () => <svg width="30" height="32" viewBox="0 0 30 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_2524_1108)">
-<path d="M15 0C8.92545 0 4 4.92545 4 11C4 17.0746 8.92545 22 15 22C21.0746 22 26 17.0746 26 11C26 4.92545 21.0746 0 15 0ZM15 20.1339C9.9567 20.1339 5.86607 16.0433 5.86607 11C5.86607 5.9567 9.9567 1.86607 15 1.86607C20.0433 1.86607 24.1339 5.9567 24.1339 11C24.1339 16.0433 20.0433 20.1339 15 20.1339Z" fill="#6D7680"/>
-<path d="M13.821 6.67857C13.821 6.99115 13.9452 7.29092 14.1662 7.51195C14.3873 7.73297 14.687 7.85714 14.9996 7.85714C15.3122 7.85714 15.612 7.73297 15.833 7.51195C16.054 7.29092 16.1782 6.99115 16.1782 6.67857C16.1782 6.36599 16.054 6.06622 15.833 5.8452C15.612 5.62417 15.3122 5.5 14.9996 5.5C14.687 5.5 14.3873 5.62417 14.1662 5.8452C13.9452 6.06622 13.821 6.36599 13.821 6.67857ZM15.5889 9.42857H14.4103C14.3023 9.42857 14.2139 9.51696 14.2139 9.625V16.3036C14.2139 16.4116 14.3023 16.5 14.4103 16.5H15.5889C15.6969 16.5 15.7853 16.4116 15.7853 16.3036V9.625C15.7853 9.51696 15.6969 9.42857 15.5889 9.42857Z" fill="#6D7680"/>
-</g>
-<defs>
-<filter id="filter0_d_2524_1108" x="0" y="0" width="30" height="32" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="6"/>
-<feGaussianBlur stdDeviation="2"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2524_1108"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2524_1108" result="shape"/>
-</filter>
-</defs>
-</svg>
+export const TokenAfterIcon = () => (
+  <svg
+    width="30"
+    height="32"
+    viewBox="0 0 30 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_d_2524_1108)">
+      <path
+        d="M15 0C8.92545 0 4 4.92545 4 11C4 17.0746 8.92545 22 15 22C21.0746 22 26 17.0746 26 11C26 4.92545 21.0746 0 15 0ZM15 20.1339C9.9567 20.1339 5.86607 16.0433 5.86607 11C5.86607 5.9567 9.9567 1.86607 15 1.86607C20.0433 1.86607 24.1339 5.9567 24.1339 11C24.1339 16.0433 20.0433 20.1339 15 20.1339Z"
+        fill="#6D7680"
+      />
+      <path
+        d="M13.821 6.67857C13.821 6.99115 13.9452 7.29092 14.1662 7.51195C14.3873 7.73297 14.687 7.85714 14.9996 7.85714C15.3122 7.85714 15.612 7.73297 15.833 7.51195C16.054 7.29092 16.1782 6.99115 16.1782 6.67857C16.1782 6.36599 16.054 6.06622 15.833 5.8452C15.612 5.62417 15.3122 5.5 14.9996 5.5C14.687 5.5 14.3873 5.62417 14.1662 5.8452C13.9452 6.06622 13.821 6.36599 13.821 6.67857ZM15.5889 9.42857H14.4103C14.3023 9.42857 14.2139 9.51696 14.2139 9.625V16.3036C14.2139 16.4116 14.3023 16.5 14.4103 16.5H15.5889C15.6969 16.5 15.7853 16.4116 15.7853 16.3036V9.625C15.7853 9.51696 15.6969 9.42857 15.5889 9.42857Z"
+        fill="#6D7680"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_2524_1108"
+        x="0"
+        y="0"
+        width="30"
+        height="32"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="6" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_2524_1108"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_2524_1108"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+);
