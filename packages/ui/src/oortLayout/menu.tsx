9eabb7a4b478bdd1c04c1b36c5ebfa4076255921
georgii.olt <olt.egor@gmail.com>
2023-08-01 12:55:27 +0300

    remove import

--- packages/ui/src/oortLayout/menu.tsx
+++ packages/ui/src/oortLayout/menu.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import styles from "./menu.module.less";
 import { Link } from "react-router-dom";
-import { Tooltip } from "antd";
 
 interface IMenuItemBtnProps {
   className?: string;
