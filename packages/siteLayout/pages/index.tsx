118117d17e4cfd3799787e4d1dd2a00a37e7f18e
georgii.olt <olt.egor@gmail.com>
2023-10-05 23:49:50 +0300

    fix test component

--- packages/siteLayout/pages/index.tsx
+++ packages/siteLayout/pages/index.tsx
@@ -1,5 +1,16 @@
+import { CSSProperties } from "react";
+
 function Home() {
-  return <>Hello</>;
+  const styles: CSSProperties = {
+    marginTop: "92px",
+    padding: "100px 0 100px 0",
+    width: "100%",
+    textAlign: "center",
+    color: "white",
+    fontSize: "36px",
+  };
+
+  return <div style={styles}>Some content</div>;
 }
 
 export default Home;
