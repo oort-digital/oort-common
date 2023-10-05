3c1d7bb8e7d330349628a83a4f8a68bd73bc3d47
georgii.olt <olt.egor@gmail.com>
2023-10-05 22:41:10 +0300

    update packages

--- packages/siteLayout/vite.config.ts
+++ packages/siteLayout/vite.config.ts
@@ -1,32 +1,31 @@
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
-import { dependencies } from "./package.json"
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { dependencies } from "./package.json";
 
 export default defineConfig({
-    plugins: [
-        react(),
-        dts({
-            insertTypesEntry: true,
-        }),
-    ],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            // name: 'MyLib',
-            formats: ['es', 'cjs'],
-            fileName: (format) => `lazy-loader-next.${format}.js`,
-        },
-        rollupOptions: {
-            external: Object.keys(dependencies),
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM'
-                },
-                sourcemap: true
-            }
-        },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
     },
+    rollupOptions: {
+      external: Object.keys(dependencies),
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+        sourcemap: true,
+      },
+    },
+  },
 });
