0702526f57e194ab24d1e3ef3fb968cc8596f626
georgii.olt <olt.egor@gmail.com>
2023-10-08 23:38:56 +0300

    fix

--- packages/siteLayout/src/layout/oortSiteLayout.tsx
+++ packages/siteLayout/src/layout/oortSiteLayout.tsx
@@ -2,16 +2,24 @@ import { LazyLoaderNextJs } from "@oort-digital/lazy-loader-next-js";
 import { ILogger } from "@oort-digital/logger";
 import dynamic, { Loader } from "next/dynamic";
 import { ReactNode } from "react";
+import { IHeaderProps } from "./desktop";
+
 interface IProps {
   logger: ILogger;
   children: ReactNode;
   deviceType?: string;
+  pathname: string;
 }
 
-const desktopLoader: Loader = () => import("./desktop");
-const mobileLoader: Loader = () => import("./mobile");
+const desktopLoader: Loader<IHeaderProps> = () => import("./desktop");
+const mobileLoader: Loader<IHeaderProps> = () => import("./mobile");
 
-export const OortSiteLayout = ({ children, logger, deviceType }: IProps) => {
+export const OortSiteLayout = ({
+  children,
+  logger,
+  deviceType,
+  pathname,
+}: IProps) => {
   const DesktopEl = dynamic(desktopLoader);
   const MobileEl = dynamic(mobileLoader);
   return (
@@ -19,9 +27,9 @@ export const OortSiteLayout = ({ children, logger, deviceType }: IProps) => {
       <LazyLoaderNextJs
         logger={logger}
         deviceType={deviceType}
-        desktop={<DesktopEl />}
-        mobile={<MobileEl />}
-        tablet={<MobileEl />}
+        desktop={<DesktopEl pathname={pathname} />}
+        mobile={<MobileEl pathname={pathname} />}
+        tablet={<MobileEl pathname={pathname} />}
       />
       {children}
     </>
