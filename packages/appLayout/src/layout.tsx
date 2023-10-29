492b66a6bf7199c73529d0adf57fa11c50612d72
georgii.olt <olt.egor@gmail.com>
2023-10-29 14:47:33 +0300

    mobile

--- packages/appLayout/src/layout.tsx
+++ packages/appLayout/src/layout.tsx
@@ -8,16 +8,14 @@ const Mobile = lazy(() => import("./layoutMobile"));
 export const Layout = (props: ILayoutProps) => {
   const deviceType = useDeviceType();
 
-  return <Desktop {...props} />;
-
-  //   const renderDevice = () => {
-  //     if (deviceType === DeviceType.Desktop) {
-  //       return <Desktop {...props} />;
-  //     }
-  //     if (deviceType === DeviceType.Phone) {
-  //       return <Mobile {...props} />;
-  //     }
-  //     return <Mobile {...props} />;
-  //   };
-  //   return <Suspense fallback={<span />}> {renderDevice()} </Suspense>;
+  const renderDevice = () => {
+    if (deviceType === DeviceType.Desktop) {
+      return <Desktop {...props} />;
+    }
+    if (deviceType === DeviceType.Phone) {
+      return <Mobile {...props} />;
+    }
+    return <Mobile {...props} />;
+  };
+  return <Suspense fallback={<span />}> {renderDevice()} </Suspense>;
 };
