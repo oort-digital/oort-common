21ad91c254056aff56d8913f1b03b693bcf64afc
georgii.olt <olt.egor@gmail.com>
2023-11-16 16:13:32 +0300

    cleanup next config

--- packages/appLayout/next.config.js
+++ packages/appLayout/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    i18n: {
-      locales: ["en"],
-      defaultLocale: "en",
-    },
-      pageExtensions:['tsx', '.js',".jsx"]
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {};
+
+module.exports = nextConfig;
