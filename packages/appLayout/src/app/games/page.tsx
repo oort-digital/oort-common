e1c90d23058ff9197243eb6c06af3d1ea861ae96
georgii.olt <olt.egor@gmail.com>
2023-11-21 21:42:06 +0300

    set page height

--- packages/appLayout/src/app/games/page.tsx
+++ packages/appLayout/src/app/games/page.tsx
@@ -1,7 +1,11 @@
 import styles from "./games.module.css";
 
 function Games() {
-  return <div className={styles.home}>Games page</div>;
+  return (
+    <div style={{ height: "2100px" }} className={styles.home}>
+      Games page
+    </div>
+  );
 }
 
 export default Games;
